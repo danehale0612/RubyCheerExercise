3a60387ce54bf474e49e818a0d3ec79bd6b77c4a
Dane Hale <danehale0612@gmail.com>
2013-04-16 09:44:04 -0500

    first commit

---
+++ yeah.rb
@@ -0,0 +1,9 @@
+puts "Hey there! What's your name?"
+name = gets.chomp
+# Give me a K!
+# Give me a e!
+# Give me a i!
+# Give me a t!
+# Give me a h!
+
+name.each_char {|answer| puts "Give me a " + answer <<"!"}
